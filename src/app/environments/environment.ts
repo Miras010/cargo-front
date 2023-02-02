fd617816f16cf9609636fbcc5eae55b1ace6ec0c
m.bayzhigit <m.bayzhigit@alterapp.net>
2023-02-02 14:59:00 +0600

    ziya dist

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -8,16 +8,16 @@ export const environment = {
   // cargoType: 'ZHAN',
 
   // AINAR
-  apiUrl: 'http://78.40.108.106:5000/api',
-  cargoType: 'AINAR',
+  // apiUrl: 'http://78.40.108.106:5000/api',
+  // cargoType: 'AINAR',
 
   // DAR
   // apiUrl: 'http://78.40.108.102:5000/api',
   // cargoType: 'DAR',
 
   // ZIYA
-  // apiUrl: 'http://195.49.215.227:5000/api',
-  // cargoType: 'ZIYA',
+  apiUrl: 'http://195.49.215.227:5000/api',
+  cargoType: 'ZIYA',
 
   // LOCALHOST
   // apiUrl: 'http://localhost:3000/api',
