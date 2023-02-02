2ff818b3b7c6ab831896a3a2e5330050b3f72a39
m.bayzhigit <m.bayzhigit@alterapp.net>
2023-02-02 14:39:06 +0600

    elm dist

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // ELMIRA
-  // apiUrl: 'http://194.110.55.17:5000/api',
-  // cargoType: 'logo',
+  apiUrl: 'http://194.110.55.17:5000/api',
+  cargoType: 'logo',
 
   // ZHAN
   // apiUrl: 'http://78.40.108.6:5000/api',
@@ -12,8 +12,8 @@ export const environment = {
   // cargoType: 'AINAR',
 
   // DAR
-  apiUrl: 'http://78.40.108.102:5000/api',
-  cargoType: 'DAR',
+  // apiUrl: 'http://78.40.108.102:5000/api',
+  // cargoType: 'DAR',
 
   // ZIYA
   // apiUrl: 'http://195.49.215.227:5000/api',
