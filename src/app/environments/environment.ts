d35552ce051f81e92012718326e6dfb043ca3225
m.bayzhigit <m.bayzhigit@alterapp.net>
2023-02-05 15:01:19 +0600

    zhan dist

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -5,9 +5,9 @@ export const environment = {
   // logoUrl: 'logo.png',
 
   // ZHAN
-  // apiUrl: 'http://78.40.108.6:5000/api',
-  // cargoType: 'ZHAN',
-  // logoUrl: 'ZHAN.jpeg',
+  apiUrl: 'http://78.40.108.6:5000/api',
+  cargoType: 'ZHAN',
+  logoUrl: 'ZHAN.jpeg',
 
 
   // AINAR
@@ -16,9 +16,9 @@ export const environment = {
   // logoUrl: 'AINAR.jpeg',
 
   // DAR
-  apiUrl: 'http://78.40.108.102:5000/api',
-  cargoType: 'DAR',
-  logoUrl: 'DAR.jpeg',
+  // apiUrl: 'http://78.40.108.102:5000/api',
+  // cargoType: 'DAR',
+  // logoUrl: 'DAR.jpeg',
 
 
   // ZIYA
